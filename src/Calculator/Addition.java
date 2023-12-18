e1ac2ea62aab7dc24364b6b4bf89f294934ababd
Joseph Beckett <josephbeckett2708@gmail.com>
2023-12-18 08:25:39 +0000

    Update 1.0
    Program works in it's most basic form and is currently the MVP
    To do:- Add other operators
    - Improve current system
    - Connect them all to a main menu

--- src/Calculator/Addition.java
+++ src/Calculator/Addition.java
@@ -2,13 +2,31 @@
 
 // Modules
 package Calculator;
+import java.util.Scanner;
 
 // Main Code
 public class Addition {
 
 	public static void main(String[] args) {
-		// Code here
-
+		// Creates two integers to be used as the main numbers
+		int num1;
+		int num2;
+		
+		// Creates an input object
+		Scanner input = new Scanner(System.in);
+		
+		// Asks for input and then assigns the input to an integer variable
+		System.out.println("Please enter the first integer: ");
+		num1 = input.nextInt();
+		
+		// Asks for input and then assigns the input to an integer variable
+		System.out.println("Please enter the Second integer: ");
+		num2 = input.nextInt();
+		
+		// Adds the two integers and prints the result
+		System.out.println("Your answer is " + (num1 + num2));
+		
+		
 	}
 
 }
