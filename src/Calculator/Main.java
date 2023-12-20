1e979b55af99d5002853c92e9f35c71acf0e2bdd
Joseph Beckett <josephbeckett2708@gmail.com>
2023-12-20 13:25:09 +0000

    Update 2.02
    Added comments
    To do:- Improve current system
    - Create a main menu
    - Add more functionalities

--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -1,10 +1,14 @@
-package Calculator;
+// A basic calculator program
 
+// Modules
+package Calculator;
 import java.util.Scanner;
 
+// Main Class
 public class Main extends Operators{
 	
 	public static void main(String[] args) {
+		// Main Code
 		// Creates two integers to be used as the main numbers
 		int num1;
 		int num2;
@@ -30,24 +34,28 @@ public class Main extends Operators{
 		System.out.print("Please enter the second integer: ");
 		num2 = input.nextInt();
 		
+		// Checks the operator
 		if (operator.equals("+")) {
 			// Prints the result
 			System.out.println("Your answer is: " + num1 + " + " + num2 + " = " + Addition(num1, num2));
+		// Checks the operator
 		} else if (operator.equals("-")) {
 			// Prints the result
 			System.out.println("Your answer is: " + num1 + " - " + num2 + " = " + Subtraction(num1, num2));
+		// Checks the operator
 		} else if (operator.equals("*")) {
 			// Prints the result
 			System.out.println("Your answer is: " + num1 + " x " + num2 + " = " + Multiplication(num1, num2));
+		// Checks the operator
 		} else if (operator.equals("/")) {
 			// Prints the result
 			System.out.println("Your answer is: " + num1 + " / " + num2 + " = " + Division(num1, num2));
+		// Checks the operator	
 		} else {
 			// Catches the wrong operator
 			System.out.println("Please enter a correct operator");
 		}
-		
-		
+				
 		// Closes input
 		input.close();
 		opInput.close();
